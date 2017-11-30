9a69cdf9453e6b2a7809c4c4a5affd915aff6111
Baraeva Ekaterina <baraeva.98@mail.ru>
2017-11-30 18:32:00 +0300

    Snapshot2, program.cpp, funcs.h,

--- program.cpp
+++ program.cpp
@@ -1,19 +1,12 @@
-//פאיכ program.cpp 
+//פאיכ program.cpp
 #include "stdafx.h"
-#include <iostream> 
+#include <iostream>
+#include "funcs.h"
 using namespace std;
-double MySum(double a, double b) 
-{ 
-	return a+b; 
-}
-double MySub(double a, double b) 
+int main()
 {
-	return a - b;
-}
-int main() 
-{ 
-	cout << MySum(10, 3) << endl; 
+	cout << MySum(10, 3) << endl;
 	cout << MySub(10, 3) << endl;
-	return 0; 
-}
+	return 0;
+}
 
